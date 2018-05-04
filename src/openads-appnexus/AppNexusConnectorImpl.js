6ce485e9599e1bf66000ffcd3d07cc8d41181f22
rafael-marquez <rafael.marquez@scmspain.com>
2018-05-04 08:50:24 +0200

    Fix tests after refactoring

--- src/openads-appnexus/AppNexusConnectorImpl.js
+++ src/openads-appnexus/AppNexusConnectorImpl.js
@@ -6,7 +6,7 @@ export default class AppNexusConnectorImpl {
   constructor ({source, connectorData, appNexusClient}) {
     this._configuration = connectorData
     this._source = source
-    this._member = this._configuration.member
+    this._member = this._configuration.Member
     this._appNexusClient = appNexusClient
     this._registeredEvents = new Map()
   }
@@ -18,23 +18,22 @@ export default class AppNexusConnectorImpl {
   get member () {
     return this._member
   }
+
   get configuration () {
     return this._configuration
   }
-  activateDebugMode () {
 
+  activateDebugMode () {
     this._appNexusClient.debug = true
     return this
   }
 
   setPageOpts ({member, keywords}) {
-
     this._appNexusClient.anq.push(() => this._appNexusClient.setPageOpts({member, keywords}))
     return this
   }
 
   onEvent ({event, targetId, callback}) {
-
     this._appNexusClient.anq.push(() => {
       this._appNexusClient.onEvent(event, targetId, callback)
       if (!this._registeredEvents.has(targetId)) {
@@ -46,25 +45,21 @@ export default class AppNexusConnectorImpl {
   }
 
   defineTag ({member, targetId, invCode, sizes, keywords, native}) {
-
     this._appNexusClient.anq.push(() => this._appNexusClient.defineTag({member, targetId, invCode, sizes, keywords, native}))
     return this
   }
 
   loadTags () {
-
     this._appNexusClient.anq.push(() => this._appNexusClient.loadTags())
     return this
   }
 
   showTag ({target}) {
-
     this._appNexusClient.anq.push(() => this._appNexusClient.showTag(target))
     return this
   }
 
   reset () {
-
     this._appNexusClient.anq.push(() => {
       this._appNexusClient.clearRequest()
       this._registeredEvents.forEach((eventArray, targetId) => {
@@ -76,13 +71,11 @@ export default class AppNexusConnectorImpl {
   }
 
   refresh (target) {
-
     this._appNexusClient.anq.push(() => this._appNexusClient.refresh(target))
     return this
   }
 
   modifyTag ({targetId, data}) {
-
     this._appNexusClient.anq.push(() => this._appNexusClient.modifyTag(targetId, data))
     return this
   }
--- src/test/openads-appnexus/AppNexusConnectorImplTest.js
+++ src/test/openads-appnexus/AppNexusConnectorImplTest.js
@@ -1,4 +1,3 @@
-
 import {expect} from 'chai'
 import sinon from 'sinon'
 import AppNexusConnectorImpl from '../../openads-appnexus/AppNexusConnectorImpl'
@@ -9,7 +8,6 @@ describe('AppNexusConnectorImpl implementation', function () {
       const source = 'AppNexus'
       const connectorData = { 'Member': 3296 }
       let appNexusClientMock = {}
-
       const appNexusConnector = new AppNexusConnectorImpl({source, connectorData, appNexusClientMock})
       let member = appNexusConnector.member
       expect(member).to.equal(3296)
@@ -18,24 +16,17 @@ describe('AppNexusConnectorImpl implementation', function () {
     it('should create a new instance of AppNexusConnectorImpl and set debug mode to true', function () {
       const source = 'AppNexus'
       const connectorData = { 'Member': 3296 }
-      const loggerSpy = sinon.spy()
       let appNexusClientMock = {
         'debug': false
-      }
-      const loggerMock = {
-        debug: (title, log) => loggerSpy()
       }
       const appNexusConnector = new AppNexusConnectorImpl({
         source,
         connectorData,
-        appNexusClient: appNexusClientMock,
-        logger: loggerMock
+        appNexusClient: appNexusClientMock
       })
       const mutatedAppNexusConnector = appNexusConnector.activateDebugMode()
-
       expect(appNexusClientMock.debug).to.be.true
       expect(mutatedAppNexusConnector).to.be.an.instanceof(AppNexusConnectorImpl)
-      expect(loggerSpy.calledOnce, 'logger debug method should be called twice, one for addPositionRenderer ').to.be.true
     })
 
     it('should create a new instance of AppNexusConnectorImpl and push setPageOpts function to the queue', function () {
@@ -43,19 +34,14 @@ describe('AppNexusConnectorImpl implementation', function () {
       const connectorData = { 'Member': 3296 }
       let appNexusQueue = []
       const qSpy = sinon.spy(appNexusQueue, 'push')
-      const loggerSpy = sinon.spy()
       let appNexusClientMock = {
         anq: appNexusQueue,
         setPageOpts: ({member, keywords}) => undefined
       }
-      const loggerMock = {
-        debug: (title, log) => loggerSpy()
-      }
       const appNexusConnector = new AppNexusConnectorImpl({
         source,
         connectorData,
-        appNexusClient: appNexusClientMock,
-        logger: loggerMock
+        appNexusClient: appNexusClientMock
       })
       const mutatedAppNexusConnector = appNexusConnector.setPageOpts({
         member: 3296,
@@ -65,7 +51,6 @@ describe('AppNexusConnectorImpl implementation', function () {
       expect(appNexusClientMock.anq).to.have.lengthOf(1)
       expect(qSpy.called).to.be.true
       expect(mutatedAppNexusConnector).to.be.an.instanceof(AppNexusConnectorImpl)
-      expect(loggerSpy.calledOnce, 'logger debug method should be called twice, one for addPositionRenderer ').to.be.true
     })
 
     it('should create a new instance of AppNexusConnectorImpl and push onEvent function to the queue', function () {
@@ -73,19 +58,14 @@ describe('AppNexusConnectorImpl implementation', function () {
       const connectorData = { 'Member': 3296 }
       let appNexusQueue = []
       const qSpy = sinon.spy(appNexusQueue, 'push')
-      const loggerSpy = sinon.spy()
       let appNexusClientMock = {
         anq: appNexusQueue,
         onEvent: ({event, targetId, callback}) => undefined
       }
-      const loggerMock = {
-        debug: (title, log) => loggerSpy()
-      }
       const appNexusConnector = new AppNexusConnectorImpl({
         source,
         connectorData,
-        appNexusClient: appNexusClientMock,
-        logger: loggerMock
+        appNexusClient: appNexusClientMock
       })
       const mutatedAppNexusConnector = appNexusConnector.onEvent({
         callback: () => undefined,
@@ -96,7 +76,6 @@ describe('AppNexusConnectorImpl implementation', function () {
       expect(appNexusClientMock.anq).to.have.lengthOf(1)
       expect(qSpy.called).to.be.true
       expect(mutatedAppNexusConnector).to.be.an.instanceof(AppNexusConnectorImpl)
-      expect(loggerSpy.calledOnce, 'logger debug method should be called twice, one for addPositionRenderer ').to.be.true
     })
 
     it('should create a new instance of AppNexusConnectorImpl and push defineTag function to the queue', function () {
@@ -104,19 +83,14 @@ describe('AppNexusConnectorImpl implementation', function () {
       const connectorData = { 'Member': 3296 }
       let appNexusQueue = []
       const qSpy = sinon.spy(appNexusQueue, 'push')
-      const loggerSpy = sinon.spy()
       let appNexusClientMock = {
         anq: appNexusQueue,
         defineTag: ({invCode, sizes, targetId}) => undefined
       }
-      const loggerMock = {
-        debug: (title, log) => loggerSpy()
-      }
       const appNexusConnector = new AppNexusConnectorImpl({
         source,
         connectorData,
-        appNexusClient: appNexusClientMock,
-        logger: loggerMock
+        appNexusClient: appNexusClientMock
       })
       const mutatedAppNexusConnector = appNexusConnector.defineTag({
         targetId: 'forlayoDiv',
@@ -127,59 +101,46 @@ describe('AppNexusConnectorImpl implementation', function () {
       expect(appNexusClientMock.anq).to.have.lengthOf(1)
       expect(qSpy.called).to.be.true
       expect(mutatedAppNexusConnector).to.be.an.instanceof(AppNexusConnectorImpl)
-      expect(loggerSpy.calledOnce, 'logger debug method should be called twice, one for addPositionRenderer ').to.be.true
     })
     it('should create a new instance of AppNexusConnectorImpl and push loadTags function to the queue', function () {
       const source = 'AppNexus'
       const connectorData = { 'Member': 3296 }
       let appNexusQueue = []
       const qSpy = sinon.spy(appNexusQueue, 'push')
-      const loggerSpy = sinon.spy()
       let appNexusClientMock = {
         anq: appNexusQueue,
         loadTags: () => undefined
       }
-      const loggerMock = {
-        debug: (title, log) => loggerSpy()
-      }
       const appNexusConnector = new AppNexusConnectorImpl({
         source,
         connectorData,
-        appNexusClient: appNexusClientMock,
-        logger: loggerMock
+        appNexusClient: appNexusClientMock
       })
       const mutatedAppNexusConnector = appNexusConnector.loadTags()
 
       expect(appNexusClientMock.anq).to.have.lengthOf(1)
       expect(qSpy.called).to.be.true
       expect(mutatedAppNexusConnector).to.be.an.instanceof(AppNexusConnectorImpl)
-      expect(loggerSpy.calledOnce, 'logger debug method should be called twice, one for addPositionRenderer ').to.be.true
     })
     it('should create a new instance of AppNexusConnectorImpl and push showTag function to the queue', function () {
       const source = 'AppNexus'
       const connectorData = { 'Member': 3296 }
       let appNexusQueue = []
       const qSpy = sinon.spy(appNexusQueue, 'push')
-      const loggerSpy = sinon.spy()
       let appNexusClientMock = {
         anq: appNexusQueue,
         showTag: ({target}) => undefined
       }
-      const loggerMock = {
-        debug: (title, log) => loggerSpy()
-      }
       const appNexusConnector = new AppNexusConnectorImpl({
         source,
         connectorData,
-        appNexusClient: appNexusClientMock,
-        logger: loggerMock
+        appNexusClient: appNexusClientMock
       })
       const mutatedAppNexusConnector = appNexusConnector.showTag({target: 'Odin'})
 
       expect(appNexusClientMock.anq).to.have.lengthOf(1)
       expect(qSpy.called).to.be.true
       expect(mutatedAppNexusConnector).to.be.an.instanceof(AppNexusConnectorImpl)
-      expect(loggerSpy.calledOnce, 'logger debug method should be called twice, one for addPositionRenderer ').to.be.true
     })
   })
   describe('Given two events registered for two different targets', () => {
@@ -188,9 +149,6 @@ describe('AppNexusConnectorImpl implementation', function () {
       this.givenEvent12 = {event: 'event1', targetId: 'target2', callback: () => null}
       this.givenEvent21 = {event: 'event2', targetId: 'target1', callback: () => null}
       this.givenEvent22 = {event: 'event2', targetId: 'target2', callback: () => null}
-      this.loggerMock = {
-        debug: (title, log) => null
-      }
     })
     describe('Registering the events', () => {
       it('Should register the events to the appnexus client', () => {
@@ -204,13 +162,10 @@ describe('AppNexusConnectorImpl implementation', function () {
           onEvent: () => null
         }
         const onEventSpy = sinon.spy(appNexusClientMock, 'onEvent')
-        const loggerSpy = sinon.spy(this.loggerMock, 'debug')
-
         const appNexusConnector = new AppNexusConnectorImpl({
           appNexusClient: appNexusClientMock,
           source: {},
-          connectorData: {},
-          logger: this.loggerMock
+          connectorData: {}
         })
         appNexusConnector
           .onEvent(this.givenEvent11)
@@ -219,7 +174,6 @@ describe('AppNexusConnectorImpl implementation', function () {
           .onEvent(this.givenEvent22)
 
         expect(onEventSpy.callCount).to.equal(4)
-        expect(loggerSpy.callCount, 'logger debug method should be called four times').to.equal(4)
       })
     })
     describe('Calling the reset method', () => {
@@ -234,13 +188,10 @@ describe('AppNexusConnectorImpl implementation', function () {
           onEvent: () => null
         }
         const offEventSpy = sinon.spy(appNexusClientMock, 'offEvent')
-        const loggerSpy = sinon.spy(this.loggerMock, 'debug')
-
         const appNexusConnector = new AppNexusConnectorImpl({
           appNexusClient: appNexusClientMock,
           source: {},
-          connectorData: {},
-          logger: this.loggerMock
+          connectorData: {}
         })
         appNexusConnector
           .onEvent(this.givenEvent11)
@@ -250,7 +201,6 @@ describe('AppNexusConnectorImpl implementation', function () {
           .reset()
 
         expect(offEventSpy.callCount).to.equal(4)
-        expect(loggerSpy.callCount, 'logger debug method should be called four times for onEvent and one more for reset method').to.equal(5)
       })
     })
   })
@@ -274,19 +224,12 @@ describe('AppNexusConnectorImpl implementation', function () {
       const defineTagSpy = sinon.spy(appNexusClientMock, 'defineTag')
       const loadTagsSpy = sinon.spy(appNexusClientMock, 'loadTags')
       const showTagSpy = sinon.spy(appNexusClientMock, 'showTag')
-      const loggerSpy = sinon.spy()
       const modifyTagSpy = sinon.spy(appNexusClientMock, 'modifyTag')
       const refreshSpy = sinon.spy(appNexusClientMock, 'refresh')
-
-      const loggerMock = {
-        debug: (title, log) => loggerSpy()
-      }
-
       const appNexusConnectorImpl = new AppNexusConnectorImpl({
         source: sourceMock,
         connectorData: connectorDataMock,
-        appNexusClient: appNexusClientMock,
-        logger: loggerMock
+        appNexusClient: appNexusClientMock
       })
 
       const givenPageOpts = {member: 1111, keywords: {p1: 'pv1'}}
@@ -302,7 +245,6 @@ describe('AppNexusConnectorImpl implementation', function () {
         .modifyTag(givenModifyTag)
         .refresh(givenRefresh)
 
-      expect(loggerSpy.callCount, 'logger debug method should be called four times').to.equal(6)
       expect(setPageOptsSpy.called).to.be.true
       expect(defineTagSpy.called).to.be.true
       expect(loadTagsSpy.called).to.be.true
